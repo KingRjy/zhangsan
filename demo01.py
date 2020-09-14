fc5098be3d40cba20abe7cbfbb02a6a151b33b1c
KingRjy <1850592707@qq.com>
2020-09-14 18:07:12 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,42 @@
+'''
+print("Hello World!")  #字符串
+print(233)  #整数
+print(2.333) #小数
+print(True)  #布尔值 True false
+print(())  #元组
+print([])  #数组
+print({})  #字典
+print("哈哈",233,2.33)
+print("哈哈"+"哼哼") #字符串得拼接
+print("哈哈"*100)
+print(((1+2)*100-99)/2)
+print(2<3)
+
+
+name = "张三" #张三赋值给名字叫a这个变量
+print(name)
+'''
+'''
+a = input("请获输入：")
+b = input("请输入：")
+print("input取得内容：",a+b)
+'''
+#数据格式得转换
+# print(type("哈哈"))
+# print(type(233))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = str("2333")
+# print(type(a))
+
+# a = float (input('请输入: '))
+# b = float (input('请输入：'))
+# print('和：',a+b)
+
+a = input('请输入: ')
+b = input('请输入：')
+print('a: ',len(a),'b: ',len(b), 'sum: ',len(a)+len(b))
